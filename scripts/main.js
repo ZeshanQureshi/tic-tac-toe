83bb48f7e5395010533d27c3f6808659447d61c3
Zeshan <zeshan@qureshi.com>
2021-10-23 19:30:51 -0400

    Created clearGame, and win condition functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,52 @@
-let box = [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0];
+let boxUsage = [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0];
+let boxValue = ["" , "" , "" , "" , "" , "" , "" , "" , "" ];
+let winner = "";
+let gameWon = 0;
 
 
 function playerInput(square) {
+    
+    clearGameStatus();
 
-    if (box[square]  == 1) {
+    if (boxUsage[square]  == 1) {
         document.getElementById("gameStatusText").innerHTML = "That square is already in use!";
+
+        //setTimeout(clearGameStatus, 2000);
+        //clearGameStatus();
 
     } else {
         
+        displayLetter(square, "X");
+
+        /*
         document.getElementById(square).innerHTML = "X";
         document.getElementById(square).style.cursor = "default";
-        box[square] = 1;
-  
-        easyComputerInput(); 
+        boxUsage[square] = 1;
+        boxValue[square] = "X";
+        */
 
+        let gameFinish = checkGameStatus(gameWon);
+
+        if (gameFinish == 1) {
+            
+            document.getElementById("gameStatusText").innerHTML = "Player Wins!";
+            endGame();
+
+        } else {
+            
+            easyComputerInput();
+            //setTimeout(easyComputerInput, 1000);
+
+            gameFinish = checkGameStatus(gameWon);
+
+            if (gameFinish == 2 ) {
+
+                document.getElementById("gameStatusText").innerHTML = "Computer Wins!";
+                endGame();
+
+            }
+        }
+         
     }
     
 }
@@ -22,17 +55,16 @@ function easyComputerInput() {
         
     let randomPick = Math.floor((Math.random() * 8) + 1);
 
-    if (box[0] == 0 || box[1] == 0 || box[2] == 0 || 
-        box[3] == 0 || box[4] == 0 || box[5] == 0 || 
-        box[6] == 0 || box[7] == 0 || box[9] == 0) {
+    if (boxUsage[0] == 0 || boxUsage[1] == 0 || boxUsage[2] == 0 || 
+        boxUsage[3] == 0 || boxUsage[4] == 0 || boxUsage[5] == 0 || 
+        boxUsage[6] == 0 || boxUsage[7] == 0 || boxUsage[9] == 0) {
     
-        while (box[randomPick] == 1) {
+        while (boxUsage[randomPick] == 1) {
             randomPick = Math.floor((Math.random() * 8) + 1);
         }
 
-        document.getElementById(randomPick).innerHTML = "O";
-        document.getElementById(randomPick).style.cursor = "default";
-        box[randomPick] = 1;
+        displayLetter(randomPick, "O");
+
     
     } else {
 
@@ -40,4 +72,64 @@ function easyComputerInput() {
 
     }
     
+}
+
+function endGame() {
+    for (i = 0; i <= 9 ; i++) {
+        document.getElementById(i).style.pointerEvents = "none";
+    }
+}
+
+function displayLetter (boxNum, text) {
+    document.getElementById(boxNum).innerHTML = text;
+    document.getElementById(boxNum).style.cursor = "default";
+    boxUsage[boxNum] = 1;
+    boxValue[boxNum] = text;
+}
+
+function clearGameStatus() {
+    document.getElementById("gameStatusText").innerHTML = "";
+}
+
+function checkGameStatus(gameCondition) {
+    
+    if (boxValue[0] == "X" && boxValue[1] == "X" && boxValue[2] == "X" ||
+        boxValue[3] == "X" && boxValue[4] == "X" && boxValue[5] == "X" ||
+        boxValue[6] == "X" && boxValue[7] == "X" && boxValue[8] == "X" ||
+        boxValue[0] == "X" && boxValue[3] == "X" && boxValue[6] == "X" ||
+        boxValue[1] == "X" && boxValue[4] == "X" && boxValue[7] == "X" ||
+        boxValue[2] == "X" && boxValue[5] == "X" && boxValue[8] == "X" ||
+        boxValue[0] == "X" && boxValue[4] == "X" && boxValue[8] == "X" ||
+        boxValue[2] == "X" && boxValue[4] == "X" && boxValue[6] == "X" ) {
+
+            gameCondition = 1;
+
+    } else if (boxValue[0] == "O" && boxValue[1] == "O" && boxValue[2] == "O" ||
+               boxValue[3] == "O" && boxValue[4] == "O" && boxValue[5] == "O" ||
+               boxValue[6] == "O" && boxValue[7] == "O" && boxValue[8] == "O" ||
+               boxValue[0] == "O" && boxValue[3] == "O" && boxValue[6] == "O" ||
+               boxValue[1] == "O" && boxValue[4] == "O" && boxValue[7] == "O" ||
+               boxValue[2] == "O" && boxValue[5] == "O" && boxValue[8] == "O" ||
+               boxValue[0] == "O" && boxValue[4] == "O" && boxValue[8] == "O" ||
+               boxValue[2] == "O" && boxValue[4] == "O" && boxValue[6] == "O") {
+
+                    gameCondition = 2;
+
+               }
+    
+
+    return gameCondition;
+}
+
+function clearGame() {
+    for (i = 0; i <= 8 ; i++) {
+        document.getElementById(i).innerHTML = "";
+        document.getElementById(i).style.cursor = "pointer";
+        document.getElementById(i).style.pointerEvents = "auto";
+
+        boxUsage[i] = 0;
+        boxValue[i] = "";
+    }
+
+    document.getElementById("gameStatusText").innerHTML = "Welcome! Click a square to begin.";
 }
