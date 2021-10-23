409095e15f7068811ea78f41e22e7e0489186e10
Zeshan <zeshan@qureshi.com>
2021-10-23 16:36:29 -0400

    Updated easyComputerInput function to avoid infinite loop

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,12 @@ function playerInput(square) {
         document.getElementById(square).style.cursor = "default";
         box[square] = 1;
 
-        easyComputerInput();
+        if (playerWin() == 1) {
+            document.getElementById("gameStatusText").innerHTML = "That square is already in use!";
+        } else {
+            easyComputerInput(); 
+
+        }
         
 
     }
@@ -23,13 +28,22 @@ function easyComputerInput() {
         
     let randomPick = Math.floor((Math.random() * 8) + 1);
 
-    while (box[randomPick] == 1) {
+    if (box[0] == 0 || box[1] == 0 || box[2] == 0 || 
+        box[3] == 0 || box[4] == 0 || box[5] == 0 || 
+        box[6] == 0 || box[7] == 0 || box[9] == 0) {
+    
+        while (box[randomPick] == 1) {
+            randomPick = Math.floor((Math.random() * 8) + 1);
+        }
 
-        randomPick = Math.floor((Math.random() * 8) + 1);
+        document.getElementById(randomPick).innerHTML = "O";
+        document.getElementById(randomPick).style.cursor = "default";
+        box[randomPick] = 1;
+    
+    } else {
+
+        document.getElementById("gameStatusText").innerHTML = "No winner!";
 
     }
-
-    document.getElementById(randomPick).innerHTML = "O";
-    document.getElementById(randomPick).style.cursor = "default";
-    box[randomPick] = 1;
+    
 }
