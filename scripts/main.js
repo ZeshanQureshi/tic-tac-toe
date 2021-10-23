4bf5d7a716ca0623c953939455357d969d0bbc8d
Zeshan <zeshan@qureshi.com>
2021-10-23 16:17:53 -0400

    Updated playerInput and easyComputerInput functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,35 @@
-let gameWon = false;
+let box = [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0];
 
+
+function playerInput(square) {
+
+    if (box[square]  == 1) {
+        document.getElementById("gameStatusText").innerHTML = "That square is already in use!";
+
+    } else {
+        
+        document.getElementById(square).innerHTML = "X";
+        document.getElementById(square).style.cursor = "default";
+        box[square] = 1;
+
+        easyComputerInput();
+        
+
+    }
+    
+}
+
+function easyComputerInput() {
+        
+    let randomPick = Math.floor((Math.random() * 8) + 1);
+
+    while (box[randomPick] == 1) {
+
+        randomPick = Math.floor((Math.random() * 8) + 1);
+
+    }
+
+    document.getElementById(randomPick).innerHTML = "O";
+    document.getElementById(randomPick).style.cursor = "default";
+    box[randomPick] = 1;
+}
